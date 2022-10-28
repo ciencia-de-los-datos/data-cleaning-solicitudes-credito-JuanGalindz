475e9f60317e73600b99f7666a8abdc76335dc91
JuanGalindz <113148335+JuanGalindz@users.noreply.github.com>
2022-10-28 18:24:30 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,8 +1,10 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
+
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
+
 """
 import pandas as pd
 import re
